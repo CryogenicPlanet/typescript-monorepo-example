b8b307eca51b480005b646e48235e67ba170b96a
Rahul Tarak <cryogenicplanet@gmail.com>
2021-06-02 21:59:00 +0530

    🎉 Base dir setup

---
+++ jest.config.base.js
@@ -0,0 +1,13 @@
+// jestconfig.base.js
+module.exports = {
+  roots: ['<rootDir>/src', '<rootDir>/__tests__'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*.(test|spec)).(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['(tests/.*.mock).(jsx?|tsx?)$'],
+  verbose: true,
+  testTimeout: 30000
+}
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+const base = require('./jest.config.base.js')
+
+module.exports = {
+  ...base,
+  projects: ['<rootDir>/packages/*/jest.config.js'],
+  coverageDirectory: '<rootDir>/coverage/'
+}
